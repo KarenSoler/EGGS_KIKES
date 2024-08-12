39fb0cce068b648c90d9df927b59631e5b9546f2
Karen Soler <kjsoler19@misena.edu.co>
2024-08-12 09:21:54 -0500

    Varible de entorno

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.eggs import eggs_router
@@ -14,3 +16,5 @@ app.add_middleware(
 
 
 app.include_router(eggs_router)
+
+
--- config/db.py
+++ config/db.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, Session
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/kikes_eggs"
+load_dotenv()
+host = os.getenv("MYSQLHOST")
+port = os.getenv("PORT")
+name = os.getenv("MYSQLDATABASE")
+user = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+
+DATABASE_URL = "mysql+pymysql://"+user+":"+password+"@"+host+":"+port+"/"+name
+
+
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
