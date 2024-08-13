24a2fa90bb55274ba1a59246ee75bb679ea81075
Karen Soler <kjsoler19@misena.edu.co>
2024-08-13 03:51:50 -0500

    engine db

--- config/db.py
+++ config/db.py
@@ -5,19 +5,19 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
+
 host = os.getenv("MYSQLHOST")
 port = os.getenv("PORT")
 name = os.getenv("MYSQLDATABASE")
 user = os.getenv("MYSQLUSER")
 password = os.getenv("MYSQLPASSWORD")
 
-DATABASE_URL = "mysql://root:YWvvCMaQTLnIPFyMhCmXqpGWfhMDpFtP@viaduct.proxy.rlwy.net:11513/railway"
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+DATABASE_URL = f"mysql://root:YWvvCMaQTLnIPFyMhCmXqpGWfhMDpFtP@viaduct.proxy.rlwy.net:11513/railway"
 
 pymysql.install_as_MySQLdb()
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 metadata = MetaData()
-metadata.bind = engine
 
 # Crear el sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -29,6 +29,5 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
 con = engine.connect()
